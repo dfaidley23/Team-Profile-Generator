5de79e4b2d3633930bdf76143b04d26551520089
David faidley <david.faidley23@gmail.com>
2022-03-01 11:44:24 -0700

    added more content to fullfil the requirements. Still building all required sections

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+const Manager = require("./lib/manager.js");
+const teamMembers = [];
 
-const employees = [];
 
-function init() {}
+
 
 init();
