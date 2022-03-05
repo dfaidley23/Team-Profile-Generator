e0d87bbf98776b8907ae095be3877132fe366697
David faidley <david.faidley23@gmail.com>
2022-03-05 12:51:01 -0700

    fixed ; missing from a few sections

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -13,8 +13,8 @@ const generateManager = function (manager) {
             </div>
         </div>
     </div>
-    `;
-}
+    `
+};
 
 // create Engineer card
 const generateEngineer = function (engineer) {
@@ -33,7 +33,7 @@ const generateEngineer = function (engineer) {
         </div>
     </div>
     `
-}
+};
 
 // create Intern card 
 const generateIntern = function (intern) {
@@ -95,7 +95,7 @@ generateHTML = (data) => {
     const generateTeam = generateTeamPage(employeeCards); 
     return generateTeam;
 
-}
+};
 
 // generate html page 
 const generateTeamPage = function (employeeCards) {   
@@ -132,8 +132,8 @@ const generateTeamPage = function (employeeCards) {
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   </html>
-`;
-}
+`
+};
 
 // export to index
 module.exports = generateHTML; 
